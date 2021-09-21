8912147f98f45d398aff1e2f56faa5f8ea44cfb2
hkcho01 <hkcho01@naver.com>
2021-09-21 21:53:28 +0900

    segmentation 1

--- filter.py
+++ filter.py
@@ -42,11 +42,21 @@ import numpy as np
 # cv2.waitKey()
 
 
+# 소벨 필터
+# src=cv2.imread("lenna.bmp",cv2.IMREAD_GRAYSCALE)
+# kernel=np.array([[-1,0,1],[-2,0,2],[-1,0,1]],dtype=np.float32) # 소벨 커널을 생성
+# gx_k=cv2.filter2D(src,-1,kernel,delta=128) # 소벨 필터 적용
+#
+# dx=cv2.Sobel(src,-1,1,0,ksize=3,delta=128)
+# dy=cv2.Sobel(src,-1,0,1,ksize=3,delta=128)
 
 
-
-
-
+# 캐니 엣지
+src=cv2.imread("lenna.bmp",cv2.IMREAD_GRAYSCALE)
+dst=cv2.Canny(src,100,150)
+cv2.imshow("src",src)
+cv2.imshow("dst",dst)
+cv2.waitKey()
 
 
 
---
+++ segmentation.py
@@ -0,0 +1,92 @@
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import glob
+import numpy as np
+import math
+
+
+# 허프 선 변환
+# src=cv2.imread("sudoku.jpg")
+# srcGray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+# src2=src.copy()
+# rows,cols=src.shape[:2]
+# edges=cv2.Canny(srcGray,100,200)
+# lines=cv2.HoughLines(edges,1,np.pi/180,130)
+# for line in lines:
+#     r,theta=line[0]
+#     tx,ty=np.cos(theta),np.sin(theta)
+#     x0,y0=tx*r,ty*r
+#     cv2.circle(src2,(abs(x0),abs(y0)),3,(0,0,255),-1)
+#     x1,y1=int(x0+cols*(-ty)),int(y0+rows*tx)
+#     x2,y2=int(x0-cols*(-ty)),int(y0-rows*tx)
+#     cv2.line(src2,(x1,y1),(x2,y2),(0,255,0),1)
+#
+# cv2.imshow("hough line",src2)
+# cv2.waitKey()
+
+
+# 확률적 허프 선 변환
+# src=cv2.imread("sudoku.jpg",cv2.IMREAD_GRAYSCALE)
+# edges=cv2.Canny(src,50,150)
+# lines=cv2.HoughLinesP(edges,1.0,np.pi/180,160,minLineLength=50,maxLineGap=5)
+# dst=cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
+#
+# for line in lines:
+#     x1,y1,x2,y2=line[0]
+#     cv2.line(dst,(x1,y1),(x2,y2),(0,0,255),2,cv2.LINE_AA)
+#
+# cv2.imshow("src",src)
+# cv2.imshow("dst",dst)
+# cv2.waitKey()
+
+
+# 허프 원 변환
+src=cv2.imread("coins_connected.jpg")
+src_gray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+blur=cv2.GaussianBlur(src_gray,(3,3),0) # 노이즈 제거를 위한 가우시안 블러
+
+def on_trackbar(pos):
+    rmin=cv2.getTrackbarPos("minRadius","src")
+    rmax=cv2.getTrackbarPos("maxRadius","src")
+    th=cv2.getTrackbarPos("threshold","src")
+
+    circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1,50,circles=None,param1=120,param2=th,minRadius=rmin,maxRadius=rmax)
+    dst=src.copy()
+
+    if circles is not None:
+        for i in range(circles.shape[1]):
+            cx,cy,radius=circles[0][i]
+            cv2.circle(dst,(cx,cy),radius,(0,0,255),2,cv2.LINE_AA)
+
+    cv2.imshow("img",dst)
+
+circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1.1,30,None,200)
+circles=np.uint16(np.around(circles))
+for i in circles[0,:]:
+    cv2.circle(src,(i[0],i[1]),i[2],(0,0,255),2)
+
+cv2.imshow("src",src)
+cv2.createTrackbar("minRadius","src",0,100,on_trackbar)
+cv2.createTrackbar("maxRadius","src",0,150,on_trackbar)
+cv2.createTrackbar("threshold","src",0,100,on_trackbar)
+cv2.setTrackbarPos("minRadius","src",10)
+cv2.setTrackbarPos("maxRadius","src",80)
+cv2.setTrackbarPos("threshold","src",40)
+cv2.waitKey()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
