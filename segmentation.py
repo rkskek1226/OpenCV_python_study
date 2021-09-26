8b6388090740634d13e4f436365e6bc1bac2536a
hkcho01 <hkcho01@naver.com>
2021-09-26 18:17:41 +0900

    segmentation 2

--- segmentation.py
+++ segmentation.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 import glob
 import numpy as np
 import math
+import random
+
+
+# 외곽선 검출
+# src=cv2.imread("contours.bmp",cv2.IMREAD_GRAYSCALE)
+# contours,hier=cv2.findContours(src,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+# dst=cv2.cvtColor(src,cv2.COLOR_GRAY2BGR)
+# idx=0
+# while idx>=0:
+#     c=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+#     cv2.drawContours(dst,contours,idx,c,4,cv2.LINE_8,hier)
+#     idx=hier[0,idx,0]
+#
+# cv2.imshow("src",src)
+# cv2.imshow("dst",dst)
+# cv2.waitKey()
 
 
 # 허프 선 변환
@@ -42,41 +58,56 @@ import math
 
 
 # 허프 원 변환
-src=cv2.imread("coins_connected.jpg")
-src_gray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-blur=cv2.GaussianBlur(src_gray,(3,3),0) # 노이즈 제거를 위한 가우시안 블러
+# src=cv2.imread("coins_connected.jpg")
+# src_gray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+# blur=cv2.GaussianBlur(src_gray,(3,3),0) # 노이즈 제거를 위한 가우시안 블러
+#
+# def on_trackbar(pos):
+#     rmin=cv2.getTrackbarPos("minRadius","src")
+#     rmax=cv2.getTrackbarPos("maxRadius","src")
+#     th=cv2.getTrackbarPos("threshold","src")
+#
+#     circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1,50,circles=None,param1=120,param2=th,minRadius=rmin,maxRadius=rmax)
+#     dst=src.copy()
+#
+#     if circles is not None:
+#         for i in range(circles.shape[1]):
+#             cx,cy,radius=circles[0][i]
+#             cv2.circle(dst,(cx,cy),radius,(0,0,255),2,cv2.LINE_AA)
+#
+#     cv2.imshow("img",dst)
+#
+# circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1.1,30,None,200)
+# circles=np.uint16(np.around(circles))
+# for i in circles[0,:]:
+#     cv2.circle(src,(i[0],i[1]),i[2],(0,0,255),2)
+#
+# cv2.imshow("src",src)
+# cv2.createTrackbar("minRadius","src",0,100,on_trackbar)
+# cv2.createTrackbar("maxRadius","src",0,150,on_trackbar)
+# cv2.createTrackbar("threshold","src",0,100,on_trackbar)
+# cv2.setTrackbarPos("minRadius","src",10)
+# cv2.setTrackbarPos("maxRadius","src",80)
+# cv2.setTrackbarPos("threshold","src",40)
+# cv2.waitKey()
 
-def on_trackbar(pos):
-    rmin=cv2.getTrackbarPos("minRadius","src")
-    rmax=cv2.getTrackbarPos("maxRadius","src")
-    th=cv2.getTrackbarPos("threshold","src")
 
-    circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1,50,circles=None,param1=120,param2=th,minRadius=rmin,maxRadius=rmax)
-    dst=src.copy()
-
-    if circles is not None:
-        for i in range(circles.shape[1]):
-            cx,cy,radius=circles[0][i]
-            cv2.circle(dst,(cx,cy),radius,(0,0,255),2,cv2.LINE_AA)
-
-    cv2.imshow("img",dst)
-
-circles=cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1.1,30,None,200)
-circles=np.uint16(np.around(circles))
-for i in circles[0,:]:
-    cv2.circle(src,(i[0],i[1]),i[2],(0,0,255),2)
-
-cv2.imshow("src",src)
-cv2.createTrackbar("minRadius","src",0,100,on_trackbar)
-cv2.createTrackbar("maxRadius","src",0,150,on_trackbar)
-cv2.createTrackbar("threshold","src",0,100,on_trackbar)
-cv2.setTrackbarPos("minRadius","src",10)
-cv2.setTrackbarPos("maxRadius","src",80)
-cv2.setTrackbarPos("threshold","src",40)
-cv2.waitKey()
-
-
-
+# 레이블링
+# src=cv2.imread("keyboard.bmp",cv2.IMREAD_GRAYSCALE)
+# _,src_bin=cv2.threshold(src,0,255,cv2.THRESH_OTSU)
+#
+# cnt,labels,stats,centroids=cv2.connectedComponentsWithStats(src_bin)
+# dst=cv2.cvtColor(src,cv2.COLOR_GRAY2BGR)
+#
+# for i in range(1,cnt): # 0은 배경이므로 제외했음
+#     x,y,w,h,a=stats[i]
+#     if a<20: # 노이즈 제거용
+#         continue
+#     cv2.rectangle(dst,(x,y,w,h),(0,0,255),2)
+#
+# cv2.imshow("src_bin",src_bin)
+# cv2.imshow("dst",dst)
+# cv2.waitKey()
 
 
 
